7c5853dead4723b4e7dd9084d106a90ac6a1711f
Zorigto Badmain <badmainzzz@mail.ru>
2020-07-01 22:34:37 +0900

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Зоригто Бадмаин on 01.07.2020.
+//  Copyright © 2020 Зоригто Бадмаин. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
